92ab39bbf85238c0b4ba21a497f77036370dac08
Ryan Lieu <github-benzbrake@woai.ru>
2020-04-13 19:35:38 +0800

    v0.1.4 Fix Bugs & remove timthumb.php

--- Plugin.php
+++ Plugin.php
@@ -1,105 +1,238 @@
 <?php
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
 /**
- * TE题图插件：<a href="http://blog.iplayloli.com/typecho-plugin-iThumb.html">发布页面</a>|<a href="https://github.com/Char1sma/iThumb">使用帮助</a>
- * 感谢<a href="http://dt27.org/">DT27</a>
- *
+ * iThumb：Typecho 题图插件 | <a href="https://doufu.ru/archives/typecho-plugin-ithumb.html">发布页面</a>
  * @package iThumb
- * @author Ryan<github-charisma@32mb.cn>
- * @version 0.1.3
- * @link http://blog.iplayloli.com/
+ * @author Ryan<github-benzbrake@woai.ru>
+ * @version 0.1.4
+ * @link https://doufu.ru
  */
- class iThumb_Plugin implements Typecho_Plugin_Interface {
-	 /**
-	 * 激活插件方法,如果激活失败,直接抛出异常
-	 *
-	 * @access public
-	 * @return String
-	 * @throws Typecho_Plugin_Exception
-	 */
-	 public static function activate()
-	{
-		Typecho_Plugin::factory('admin/write-post.php')->option = array('iThumb_Plugin', 'addOption');
-		Typecho_Plugin::factory('admin/write-post.php')->bottom = array('iThumb_Plugin', 'addFooter');
-	}
+class iThumb_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return String
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        if (array_key_exists('ArticleImg', Typecho_Plugin::export()['activated'])) {
+            throw new Typecho_Plugin_Exception('启用失败，与 ArticleImg 插件不兼容');
+        }
+
+        // 文章编辑
+        Typecho_Plugin::factory('admin/write-post.php')->option = array(__CLASS__, 'addOption');
+        Typecho_Plugin::factory('admin/write-post.php')->bottom = array(__CLASS__, 'addFooter');
+        Typecho_Plugin::factory('Widget_Contents_Post_Edit')->finishPublish = array(__CLASS__, "applyThumb");
+
+        // 页面编辑
+        Typecho_Plugin::factory('admin/write-page.php')->option = array(__CLASS__, 'addOption');
+        Typecho_Plugin::factory('admin/write-page.php')->bottom = array(__CLASS__, 'addFooter');
+        Typecho_Plugin::factory('Widget_Contents_Page_Edit')->finishPublish = array(__CLASS__, "applyThumb");
+
+        // 前台
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->filter = array(__CLASS__, 'render');
+    }
 /**
-	 * 禁用插件方法,如果禁用失败,直接抛出异常
-	 *
-	 * @static
-	 * @access public
-	 * @return void
-	 * @throws Typecho_Plugin_Exception
-	 */
-	public static function deactivate(){}
-	/**
-	 * 获取插件配置面板
-	 *
-	 * @access public
-	 * @param Typecho_Widget_Helper_Form $form 配置面板
-	 * @return void
-	 */
-	public static function config(Typecho_Widget_Helper_Form $form){}
-	/**
-	 * 个人用户的配置面板
-	 *
-	 * @access public
-	 * @param Typecho_Widget_Helper_Form $form
-	 * @return void
-	 */
-	public static function personalConfig(Typecho_Widget_Helper_Form $form){}
-	/**
-	 * 附加JS和CSS
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public static function addOption()
-	{
-		echo '<section class="typecho-post-option">
-	<label for="token-input-tags" class="typecho-label">题图</label>
-	<div id="thumb-preview-area" style="border:3px dashed #D9D9D6;background-color:#FFF;color:#999;min-height: 35px;" class="p">
-	<a class="selectThumb">选择图片</a>
-	<a class="deleteThumb hide">删除图片</a>
+ * 禁用插件方法,如果禁用失败,直接抛出异常
+ *
+ * @static
+ * @access public
+ * @return void
+ * @throws Typecho_Plugin_Exception
+ */
+    public static function deactivate()
+    {}
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $defaultUrl = new Typecho_Widget_Helper_Form_Element_Text('default_image', null, _t(''), _t('默认文章图片URL'), _t('在这里输入默认的图片URL，示例<code>https://ooo.0o0.ooo/2017/02/13/58a165406ce28.png</code>'));
+        $form->addInput($defaultUrl);
+    }
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {}
+    /**
+     * 把题图写入数据库
+     *
+     * @return void
+     * @date 2020-04-12
+     */
+    public static function applyThumb($contents, $post)
+    {
+        $thumbUrl = $post->request->get('ithumb-url', '');
+        self::setField('thumb', 'str', $thumbUrl, $post->cid);
+    }
+    /**
+     * 获取所有字段
+     *
+     * @access public
+     * @param int $cid
+     * @return Typecho_Config
+     */
+    private static function getFields($cid)
+    {
+        $db = Typecho_Db::get();
+        $fields = array();
+        $rows = $db->fetchAll($db->select()->from('table.fields')
+                ->where('cid = ?', $cid));
+
+        foreach ($rows as $row) {
+            $fields[$row['name']] = $row[$row['type'] . '_value'];
+        }
+        return new Typecho_Config($fields);
+    }
+    /**
+     * 设置单个字段
+     *
+     * @param string $name
+     * @param string $type
+     * @param string $value
+     * @param integer $cid
+     * @access private
+     * @return integer
+     */
+    private function setField($name, $type, $value, $cid)
+    {
+        $db = Typecho_Db::get();
+        if (empty($name) || !in_array($type, array('str', 'int', 'float'))) {
+            return false;
+        }
+
+        $exist = $db->fetchRow($db->select('cid')->from('table.fields')
+                ->where('cid = ? AND name = ?', $cid, $name));
+
+        if (empty($exist)) {
+            return $db->query($db->insert('table.fields')
+                    ->rows(array(
+                        'cid' => $cid,
+                        'name' => $name,
+                        'type' => $type,
+                        'str_value' => 'str' == $type ? $value : null,
+                        'int_value' => 'int' == $type ? intval($value) : 0,
+                        'float_value' => 'float' == $type ? floatval($value) : 0,
+                    )));
+        } else {
+            return $db->query($db->update('table.fields')
+                    ->rows(array(
+                        'type' => $type,
+                        'str_value' => 'str' == $type ? $value : null,
+                        'int_value' => 'int' == $type ? intval($value) : 0,
+                        'float_value' => 'float' == $type ? floatval($value) : 0,
+                    ))
+                    ->where('cid = ? AND name = ?', $cid, $name));
+        }
+    }
+    /**
+     * 附加JS和CSS
+     *
+     * @access public
+     * @return void
+     */
+    public static function addOption($post)
+    {
+        $fields = self::getFields($post->cid);
+        $thumb = "";
+        if (isset($fields->thumb)) {
+            $thumb = $fields->thumb;
+        }
+        ?>
+<section class="typecho-post-option">
+	<label for="ithumb-url" class="typecho-label"><?php _e('题图');?></label>
+	<p><input id="ithumb-url" name="ithumb-url" type="text" value="<?php echo $thumb; ?>" class="hide w-100 text" /></p>
+	<div id="thumb-preview-area">
+		<?php if ($thumb !== ""): ?>
+		<div class="thumb-preview-area-header">
+			<a class="selectThumb"><?php _e('选择图片');?></a>
+			<a class="deleteThumb"><?php _e('删除图片');?></a>
+		</div>
+		<div>
+			<img src="<?php echo $thumb; ?>" />
+		</div>
+		<?php else: ?>
+		<div class="thumb-preview-area-header">
+			<a class="selectThumb"><?php _e('选择图片');?></a>
+			<a class="deleteThumb hide"><?php _e('删除图片');?></a>
+		</div>
+		<?php endif;?>
 	</div>
-</section>';
-	}
-	public static function addFooter()
-	{
-?>
-<div id="thumbPanel" class="hide">
-<div class='outerPanel'>
-</div>
-<div class="innerPanel">
-<h1 class="nomp"><?php _e('题图'); ?></h1>
-<button type="button" class="thumb-panel-close">X</button>
-<div id="ListOnThumbPanel">
-</div>
-</div>
+</section>
+<?}
+    public static function addFooter()
+    {
+        ?>
+<div id="thumb-panel" class="hide">
+	<div class='outer-panel'>
+		<div class="inner-panel">
+			<header class="thumb-panel-header clearfix">
+				<h1 class="thumb-panel-title"><?php _e('题图');?></h1>
+				<div class="right">
+					<a id="upload-link" class="thumb-panel-btn">上传</a>
+					<button type="button" class="thumb-panel-btn thumb-panel-close">X</button>
+				</div>
+			</header>
+			<section id="thumb-list-panel">
+			</section>
+		</div>
+	</div>
 </div>
 <style>
 .hide {
 	display:none;
 }
-.nomp {
-	padding: 0px;
-	margin: 0px;
+#thumb-preview-area {
+	border: 1px solid #D9D9D6;
+    max-height: 240px;
+    overflow: auto;
+    background-color: #FFF;
+    border-radius: 2px;
+	margin: 1em 0;
 }
-#thumb-preview-area > a:hover {
+#thumb-preview-area > div {
+	padding: 6px 12px;
+}
+#thumb-preview-area .thumb-preview-area-header {
+	margin-bottom: .5em;
+	border-bottom: 1px solid #D9D9D6;
+	display: flex;
+}
+#thumb-preview-area .thumb-preview-area-header .deleteThumb {
+	margin-left: auto;
+}
+#thumb-preview-area img {
+	width: 100%;
+}
+#thumb-preview-area .thumb-preview-area-header a:hover {
 	cursor:pointer;
 }
-.outerPanel {
+#thumb-panel .outer-panel {
 	position: fixed;
 	top: 0px;
 	left: 0px;
 	right: 0px;
 	bottom: 0px;
 	z-index: 300;
-	background: #000 none repeat scroll 0% 0%;
-	opacity: 0.7;
+	background-color: rgba(0,0,0, .7);
 }
-.innerPanel {
+#thumb-panel .inner-panel {
 	background: #f3f3f3;
-	z-index: 301;
 	position: fixed;
 	top: 30px;
 	right: 30px;
@@ -107,112 +240,175 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 	bottom: 30px;
 	padding: 20px;
 }
-.thumb-panel-close {
-	position: fixed;
-	right: 50px;
-	top: 50px;
+#thumb-panel .thumb-panel-header {
+	display: flex;
+	align-items: center;
+}
+#thumb-panel .thumb-panel-title {
+    margin: 0;
+}
+#thumb-panel .thumb-panel-header .right {
+	margin-left: auto;
+}
+#thumb-panel .thumb-panel-btn {
 	border: none;
+	padding: 5px 10px;
+	background-color: #E9E9E6;
+	color: #666;
+	border: 2px solid #ccc;
+    -moz-border-radius: 2px;
+    -webkit-border-radius: 2px;
+    border-radius: 2px;
+	outline: none;
+	transition: background-color .2s;
+	text-decoration: none;
+}
+#thumb-panel #upload-link {
+	display: none;
+}
+#thumb-panel .thumb-panel-btn:hover {
+    background-color: #467B96;
+    cursor: pointer;
+    color: #FFF;
+}
+#thumb-panel .thumb-panel-close:hover {
+	background-color: coral;
 }
 #uploadlink {
-    position: fixed;
-    top: 70px;
-    left: 8em;
+	display: none;
 }
-#ListOnThumbPanel ul {}
-#ListOnThumbPanel ul>li {
-	display:inline-block;
-	margin:5px;
-	border:1px dashed;
+#thumb-list-panel {
+	border:3px dashed #D9D9D6;
+	background-color:#FFF;
+	color:#999;
+	min-height: 35px;
 }
-#ListOnThumbPanel ul>li>*:hover {
-	cursor:pointer;
+#thumb-list-panel ul {
+    margin: 0;
+    padding: 1em;
+    width: 100%;
+    box-sizing: border-box;
+	display: flex;
+	flex-flow: wrap;
+}
+#thumb-list-panel ul > li {
+	display: block;
+	width: 25%;
+}
+#thumb-list-panel ul > li .img {
+    background-size: cover;
+    height: 240px;
+    border: 1px dashed;
+    margin: .5em;
+	display: flex;
+	flex-direction: column-reverse;
+	align-items: center;
+}
+#thumb-list-panel ul > li .img:hover {
+	cursor: pointer;
+}
+#thumb-list-panel ul > li cite {
+    font-style: normal;
+    padding: .5em;
+    display: block;
+    background: rgba(0, 0, 0, .45);
+    width: 100%;
+    text-align: center;
+    color: #fff;
 }
 </style>
 	<script>
         $(document).ready(function () {
-			timThumb = "<?php echo Helper::options()->pluginUrl.'/iThumb/timthumb.php'; ?>";
+			$(":input[name='fields[thumb]']", $("#custom-field")).parents('tr').hide();
+			$(":input[value='thumb']", $("#custom-field")).parents('tr').hide();
 			$('.thumb-panel-close').click(function() {
-				$('#thumbPanel').addClass('hide');
+				$('#thumb-panel').addClass('hide');
 			});
+			// 在题图框预览图片
 			function previewThumb(url) {
-				thumbimg = $('img',$('#thumb-preview-area'));
+				var thumbimg = $('img',$('#thumb-preview-area'));
 				if (thumbimg.length) {
-					thumbimg.attr('src',timThumb + '?src=' + url +'&h=250&w=250&zc=1');
+					thumbimg.attr('src', url);
 				} else {
-					$('#thumb-preview-area').append('<img src="'+ timThumb + '?src=' + url +'&h=250&w=250&zc=1" />');
+					$('#thumb-preview-area').append('<div><img src="'+ url + '"/></div>');
 				}
-			}
-			function refleshThumbList() {
-				thumbs = $('img',$('#ListOnThumbPanel'));
-				if (thumbs.length) thumbs.parent().parent().remove();
-				fileList = $('li',$('#file-list'));
-				ListOnThumbPanel = $('#ListOnThumbPanel');
-				ThumbListHtml = "<ul>";
-				for(ttt=0;ttt<fileList.length;ttt++) {
-					var link = fileList.eq(ttt).attr('data-url');
-					var name = $('a',fileList.eq(ttt)).eq(0).text();
-					var ext = link.substring(link.lastIndexOf(".")).toUpperCase();
-					if (ext=='.JPG'||ext=='.JPEG'||ext=='.PNG'||ext=='.GIF'||ext=='.TIFF') 
-						ThumbListHtml = ThumbListHtml +'<li data-url="'+ link +'"><img src="'+ timThumb + '?src=' + link +'&h=250&w=250&zc=1" /></li>';
-				}
-				ThumbListHtml = ThumbListHtml + "</ul>";
-				ListOnThumbPanel.append(ThumbListHtml);
-				$('img',$('#ListOnThumbPanel')).click(function() {
-				$("[value='thumbUrl']").parent().parent().children('td:eq(2)').children('textarea').val($(this).parent().attr('data-url'));
-				previewThumb($(this).parent().attr('data-url'));
 				$('.deleteThumb').removeClass('hide');
-				$('#thumbPanel').addClass('hide');
-			});
 			}
+			// 刷新图片选择框中的图片
+			function refleshAttachThumbs() {
+				var panel = $('#thumb-list-panel');
+				var thumbs = $('.img', panel);
+				if (thumbs.length) thumbs.parents('ul').remove();
+				var fileList = $('li', $('#file-list'));
+				var html = "<ul>";
+				for(times=0; times < fileList.length; times++) {
+					var link = fileList.eq(times).data('url');
+					var isImage = fileList.eq(times).data('image');
+					var cid = fileList.eq(times).data('cid');
+					var name = $('a',fileList.eq(times)).eq(0).text();
+					if (isImage == 1)
+						html +='<li data-cid="' + cid +'" data-url="'+ link +'"><div class="img" style="background-image:url(' + link + ')"><cite>' + name + '</cite></div></li>';
+				}
+				html += "</ul>";
+				panel.append(html);
+				// 设置题图
+				$('.img', panel).click(function() {
+					$("#ithumb-url").val($(this).parents('li').data('url'));
+					previewThumb($(this).parents('li').data('url'));
+					$('.deleteThumb').removeClass('hide');
+					$('#thumb-panel').addClass('hide');
+				});
+			}
+			// 显示图片选择框
 			$('.selectThumb').click(function() {
-				refleshThumbList();
-				$('#thumbPanel').removeClass('hide');
+				refleshAttachThumbs();
+				$('#thumb-panel').removeClass('hide');
 			})
+			// 展开自定义字段
             var btn = $('i', $('#custom-field-expand'));
             if (btn.hasClass('i-caret-right')) {
                 btn.removeClass('i-caret-right').addClass('i-caret-down');
                 $('#custom-field-expand').parent().removeClass('fold');
             }
-            if (!$('[name="fieldNames[]"]').is("[value='thumbUrl']")) {
-                if($('[name="fieldNames[]"]').last().val()!=""){
-                    var html = '<tr><td><input type="text" name="fieldNames[]" placeholder="<?php _e('字段名称'); ?>" class="text-s w-100"></td>'
-                            + '<td><select name="fieldTypes[]" id="">'
-                            + '<option value="str"><?php _e('字符'); ?></option>'
-                            + '<option value="int"><?php _e('整数'); ?></option>'
-                            + '<option value="float"><?php _e('小数'); ?></option>'
-                            + '</select></td>'
-                            + '<td><textarea name="fieldValues[]" placeholder="<?php _e('字段值'); ?>" class="text-s w-100" rows="2"></textarea></td>'
-                            + '<td><button type="button" class="btn btn-xs"><?php _e('删除'); ?></button></td></tr>',
-                        el = $(html).hide().appendTo('#custom-field table tbody').fadeIn();
-                    attachDeleteEvent(el);
-                }
-                $('[name="fieldTypes[]"]').last().find("option[value='str']").attr("selected",true).siblings().remove();
-                $('[name="fieldNames[]"]').last().attr('value', 'thumbUrl').attr('readonly','readonly');
-                $('[name="fieldValues[]"]').last().attr('placeholder','文章缩略图，推荐尺寸：700px*220px');
-                var html = '<tr><td><input type="text" name="fieldNames[]" placeholder="<?php _e('字段名称'); ?>" class="text-s w-100"></td>'
-                        + '<td><select name="fieldTypes[]" id="">'
-                        + '<option value="str"><?php _e('字符'); ?></option>'
-                        + '<option value="int"><?php _e('整数'); ?></option>'
-                        + '<option value="float"><?php _e('小数'); ?></option>'
-                        + '</select></td>'
-                        + '<td><textarea name="fieldValues[]" placeholder="<?php _e('字段值'); ?>" class="text-s w-100" rows="2"></textarea></td>'
-                        + '<td><button type="button" class="btn btn-xs"><?php _e('删除'); ?></button></td></tr>',
-                    el = $(html).hide().appendTo('#custom-field table tbody').fadeIn();
-                attachDeleteEvent(el);
-            }else{
-				var thumbarea = $("[value='thumbUrl']").parent().parent().children('td:eq(2)').children('textarea');
-                thumbarea.attr('placeholder','文章缩略图，推荐尺寸：700px*220px');
-				if (thumbarea.val()!='') previewThumb(thumbarea.val());
-				$('.deleteThumb').removeClass('hide');
-            }
+			// 删除题图
 			$('.deleteThumb').click(function() {
 				$('.deleteThumb').addClass('hide');
-				$('img',$('#thumb-preview-area')).remove();
-				$("[value='thumbUrl']").parent().parent().children('td:eq(2)').children('textarea').val('');
+				$('img',$('#thumb-preview-area')).parent().remove();
+				$("#ithumb-url").val('');
 			});
         });
 	</script>
 <?php
-	}
- }
- ?>
+}
+    /**
+     * 获取题图
+     *
+     * @param int $cid
+     * @return String
+     * @date 2020-04-12
+     */
+    public static function render($value, $archive)
+    {
+        if (!(isset($value['thumb']))) {
+            $value['thumb'] = "";
+        }
+        if (empty($value['thumb'])) {
+            preg_match_all("/<[img|IMG].*?src=[\'|\"](.*?)[\'|\"].*?[\/]?>/", $archive->content, $matches);
+            if (isset($matches[1])) {
+                foreach ($matches[1] as $v) {
+                    if (strpos($thumb, __TYPECHO_PLUGIN_DIR__ . "/TePass") !== false) {
+                        continue;
+                    }
+                    $value['thumb'] = $v;
+                }
+            }
+        }
+        if (empty($value['thumb'])) {
+            $value['thumb'] = Helper::options()->plugin('iThumb')->default_image;
+        }
+        if (empty($value['thumb'])) {
+            $value['thumb'] = Helper::options()->pluginUrl . '/iThumb/images/' . mt_rand(0, 9) . '.jpg';
+        }
+        return $value;
+    }
+}
--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 ## iThumb
 Typecho文章缩略图(Post Thumbnail)插件
-基于[SlantedExtend](https://github.com/DT27/SlantedExtend "SlantedExtend")的`thumbUrl`自定义字段增强增加了图片选择框。图片选择是从当前文章附件列表选取的，目前不支持从上传的所有文件中选取
+~~基于[SlantedExtend](https://github.com/DT27/SlantedExtend "SlantedExtend")的`thumbUrl`自定义字段增强增加了图片选择框。~~
+由于和主题自定义字段冲突，导致本插件使用JS设置题图自定义字段不生效，目前版本改成数据库查询方式修改自定义字段。
+图片选择是从当前文章附件列表选取的，目前不支持从上传的所有文件中选取。
+### Version
+> 0.1.4 修改自定义字段名称，修改自定义字段提交方式，新增默认题图。
+
 ### Usage
 插件目录必须为iThumb
 前台通过自定义字段获取缩略图链接
 ```php
-<?php $this->fields->thumbUrl; ?>
+<?php $this->thumb(); ?>
+缩略图获取顺序：设置的题图->文章图片->插件配置里的默认图片->随机图片
 ```
 ### Thanks
 [DT27](https://dt27.org/ "DT27")
 [TimThumb](https://www.binarymoon.co.uk/projects/timthumb/ "TimThumb")
 ### Plugin License
->  Copyright © 2016 Ryan
+>  Copyright © 2016-2020 Ryan
 >  [Mozilla Public License Version 2.0](https://www.mozilla.org/en-US/MPL/2.0/ "Mozilla Public License Version 2.0")
 
 ### Preview
-![](http://blog.iplayloli.com/usr/uploads/2016/04/2194918340.gif)
+![](https://s1.ax1x.com/2020/04/12/GONy5T.gif)
 
-![](http://blog.iplayloli.com/usr/uploads/2016/04/684669895.gif)
+![](https://s1.ax1x.com/2020/04/12/GONsaV.gif)
 
-![](http://blog.iplayloli.com/usr/uploads/2016/04/3652509993.gif)
+![](https://s1.ax1x.com/2020/04/12/GONrV0.gif)
